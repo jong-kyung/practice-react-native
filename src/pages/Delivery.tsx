b3774702d8b07d2a88cd00f46a5e8b35d8ac590f
이종경 <gyoung3063413@naver.com>
2024-07-31 19:37:30 +0900

    feat: Delivery 컴포넌트 스택 라우팅 적용

--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -1,10 +1,19 @@
-import {Text, View} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Ing from './Ing';
+import Complete from './Complete';
+
+const Stack = createNativeStackNavigator();
 
 function Delivery() {
   return (
-    <View>
-      <Text>주문</Text>
-    </View>
+    <Stack.Navigator initialRouteName="Ing">
+      <Stack.Screen name="Ing" component={Ing} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Complete"
+        component={Complete}
+        options={{headerShown: false}}
+      />
+    </Stack.Navigator>
   );
 }
 
